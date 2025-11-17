593997631ef1e5d0d922cfc5937ea415e297324a
miguelRodr1gues <miguel.rodrigues103@hotmail.com>
2025-11-17 10:51:04 +0000

    first version

---
+++ src/main/java/pt/ulusofona/cd/restaurant/RestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+package pt.ulusofona.cd.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantServiceA22306155A22304759Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantServiceA22306155A22304759Application.class, args);
+	}
+
+}
---
+++ src/test/java/pt/ulusofona/cd/restaurant/RestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ulusofona.cd.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantServiceA22306155A22304759ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
